9d728c126c2c132542d0d08b47f52398aee74f8b
Gary Lai <garylai@MacBook-Air-de-Gary.local>
2019-08-26 22:46:22 -0500

    updates tracking ID for google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // replace "UA-XXXXXXXXX-X" with your own Tracking ID
-        trackingId: "UA-XXXXXXXXX-X"
+        trackingId: "UA-146500156-1"
       }
     },
     "gatsby-transformer-sharp",
