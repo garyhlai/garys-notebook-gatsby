06e762d4e6e7a4d6b3fd4092ee409ff9b196a73b
Gary Lai <garylai@MacBook-Air-de-Gary.local>
2019-12-05 14:48:54 -0500

    .

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,18 +16,18 @@ const AboutPage = ({ data }) => {
         <p>Hi folks, </p>
 
         <p>
-          My name is Gary Haizhi Lai and I am an indie maker{" "}
+          My name is Gary Haizhi Lai and I am a maker{" "}
           <i class="em em-hammer_and_wrench" /> interested in machine learning
           and blockchain.
         </p>
 
         <p>
           I’m starting this blog here in order to share my notes on
-          miscellaneous programming topics as I come across them.{" "}
+          miscellaneous ML and programming topics as I come across them.{" "}
           <i class="em em-nerd_face" /> By explaining things, I can understand
           them better while providing values to others. Meanwhile, I hope to
-          connect with and learn from fellow programmers and travelers who find
-          my blog posts useful. <i class="em em-handshake" />
+          connect with and learn from fellow programmers who find my blog posts
+          useful. <i class="em em-handshake" />
         </p>
         <p>
           I will start a regular newsletter soon - subscribe if you’d like to
